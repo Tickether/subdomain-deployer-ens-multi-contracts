9ca072d522a2f2a35254c51e85ed8e652bf3e670
Tickether <tickether@gmail.com>
2023-07-05 11:41:10 +0000

    Initial commit

---
+++ contracts/StringUtils.sol
@@ -0,0 +1,82 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.4;
+
+library StringUtils {
+    /**
+     * @dev Returns the length of a given string
+     *
+     * @param s The string to measure the length of
+     * @return The length of the input string
+     */
+    function strlen(string memory s) internal pure returns (uint256) {
+        uint256 len;
+        uint256 i = 0;
+        uint256 bytelength = bytes(s).length;
+        for (len = 0; i < bytelength; len++) {
+            bytes1 b = bytes(s)[i];
+            if (b < 0x80) {
+                i += 1;
+            } else if (b < 0xE0) {
+                i += 2;
+            } else if (b < 0xF0) {
+                i += 3;
+            } else if (b < 0xF8) {
+                i += 4;
+            } else if (b < 0xFC) {
+                i += 5;
+            } else {
+                i += 6;
+            }
+        }
+        return len;
+    }
+
+    function validateString(string memory label) internal pure returns (bool) {
+        bytes memory inputBytes = bytes(label);
+        
+        // Check if the string is empty
+        if (inputBytes.length == 0) {
+            return false;
+        }
+        
+        // Check the first character of the string
+        if (!((inputBytes[0] >= 0x30 && inputBytes[0] <= 0x39) || (inputBytes[0] >= 0x61 && inputBytes[0] <= 0x7A))) {
+            return false;
+        }
+        
+        // Check the remaining characters of the string
+        for (uint256 i = 1; i < inputBytes.length; i++) {
+            bytes1 char = inputBytes[i];
+
+            // Check for spaces within the input string
+            if (char == 0x20) {
+            return false;  // Space character is not allowed
+            }
+            
+            // Check if the character is a lowercase letter or a number
+            if (!((char >= 0x30 && char <= 0x39) || (char >= 0x61 && char <= 0x7A))) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    function extractLabel(bytes memory encodedLabel) 
+        internal 
+        pure 
+        returns (string memory) 
+    {
+        require(encodedLabel.length >= 1, "Invalid encoded label");
+
+        uint8 labelLength = uint8(encodedLabel[0]);
+        require(encodedLabel.length >= labelLength + 1, "Invalid encoded label");
+
+        bytes memory labelBytes = new bytes(labelLength);
+        for (uint8 i = 0; i < labelLength; i++) {
+            labelBytes[i] = encodedLabel[i + 1];
+        }
+
+        return string(labelBytes);
+    }
+}
---
+++ contracts/ensSubDomainer.sol
@@ -0,0 +1,264 @@
+//SPDX-License-Identifier: UNLICENSED
+//Code by @0xGeeLoko
+
+
+pragma solidity ^0.8.17;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./StringUtils.sol";
+
+//interfaces
+/*
+*ENS
+*/
+interface IENS {
+    function owner(bytes32 node) external view returns (address);
+
+    function resolver(bytes32 node) external view returns (address);
+
+    function recordExists(bytes32 node) external view returns (bool);
+
+    function isApprovedForAll(
+        address owner,
+        address operator
+    ) external view returns (bool);
+}
+
+
+/*
+*BaseRegistrarImplementation
+*/
+interface IBaseRegistrarImplementation {
+    function nameExpires(uint256 id) external view returns (uint256);
+}
+
+
+/*
+*NameWrapper
+*/
+interface INameWrapper {
+    function isWrapped(bytes32) external view returns (bool);
+    
+    function names(bytes32) external view returns (bytes memory);
+    
+    function setSubnodeRecord(
+        bytes32 node,
+        string calldata label,
+        address owner,
+        address resolver,
+        uint64 ttl,
+        uint32 fuses,
+        uint64 expiry
+    ) external returns (bytes32);
+
+    function setFuses(
+        bytes32 node,
+        uint16 ownerControlledFuses
+    ) external returns (uint32 newFuses);
+    
+    function extendExpiry(
+        bytes32 node,
+        bytes32 labelhash,
+        uint64 expiry
+    ) external returns (uint64);
+
+    function getData(
+        uint256 id
+    ) external view returns (address, uint32, uint64);
+
+    function allFusesBurned(
+        bytes32 node,
+        uint32 fuseMask
+    ) external view returns (bool);
+
+    function isApprovedForAll(
+        address account,
+        address operator
+    ) external view returns (bool);
+}
+
+
+
+contract EnsSubDomain is Ownable, ReentrancyGuard {
+    AggregatorV3Interface internal priceFeed = AggregatorV3Interface( 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e ); // change beefore mainnet
+    
+
+    IENS ens = IENS(0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e);
+    INameWrapper nameWrapper = INameWrapper(0x114D4603199df73e7D157787f8778E21fCd13066);
+    IBaseRegistrarImplementation baseRegistrarImplementation = IBaseRegistrarImplementation(0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85);
+    
+    bytes32 public parentNode;
+    address public ensResolver;
+
+    uint256 public threeUpLetterFee;
+    uint256 public fourFiveLetterFee;
+    uint256 public sixDownLetterFee;
+
+    //track eth balances mapping per node
+    //track prices mapping per node
+    //payout wallets per node
+
+
+    // set base fee
+    function setLetterFees(uint256 threeUpLetterFee_, uint256 fourFiveLetterFee_, uint256 sixDownLetterFee_)  
+        external 
+    {
+        threeUpLetterFee = threeUpLetterFee_;
+        fourFiveLetterFee = fourFiveLetterFee_;
+        sixDownLetterFee = sixDownLetterFee_;
+        
+    }
+
+    /**
+     * Returns the latest price.
+     */
+    // usd/eth chainlink oracle
+    function getLatestPrice(uint256 usdPrice) 
+        internal 
+        view
+        returns (int) 
+    
+    {
+        (
+            /* uint80 roundID */,
+            int etherPrice,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+        return int(usdPrice) * 1000000000000000000 / (etherPrice);
+    }
+
+    
+    //gets price base on string length
+    function getLetterFees(string memory label, uint256 duration)  
+        public
+        view
+        returns (uint256) 
+    {
+        uint len = StringUtils.strlen(label);
+        require(len > 0);
+
+        uint256 price;
+        if (len <= 3) {
+            price = uint256(getLatestPrice(threeUpLetterFee)) * duration;
+        } else if (len <= 5) {
+            price = uint256(getLatestPrice(fourFiveLetterFee)) * duration;
+        } else if (len >= 6) {
+            price = uint256(getLatestPrice(sixDownLetterFee)) * duration;    
+        }
+        return price;
+    }
+
+    
+
+   modifier isApprovedLabel (string memory subNodeLabel_) {
+        require(StringUtils.validateString(subNodeLabel_), "not a valid string");
+        _;
+    }
+
+    modifier isAvailableLabel (string memory subNodeLabel_) {
+        bytes32 labelhash = keccak256(bytes(subNodeLabel_));
+        require(!ens.recordExists(keccak256(abi.encodePacked(parentNode, labelhash))), "not Available Label");
+        _;
+    }
+
+    modifier isNodeActiveOwnerorApproved (bytes32 node) {
+        (address owner, uint32 fuses, uint64 expiry) = nameWrapper.getData(uint256(node));
+        if(nameWrapper.isWrapped(node)) {
+            require(msg.sender == owner || nameWrapper.isApprovedForAll(owner, msg.sender), "not the owner of node");
+        } else {
+            require(msg.sender == ens.owner(node) || ens.isApprovedForAll(ens.owner(node), msg.sender), "not the owner of node");
+        }
+        _;
+    }
+
+//function to set initial domain and node
+
+    function setBaseEns(bytes32 node)
+        onlyOwner  
+        isNodeActiveOwnerorApproved(node)
+        external 
+    {
+        parentNode = node;
+        ensResolver = ens.resolver(parentNode);
+    }
+
+
+
+    
+    function getParentExpiry()  
+        internal
+        view
+        returns (uint256) 
+    {
+        string memory label = StringUtils.extractLabel(nameWrapper.names(parentNode));
+        uint256 tokenId = uint256(keccak256(bytes(label)));
+        return baseRegistrarImplementation.nameExpires(tokenId);
+    }
+
+// function to set new sub domain
+
+    function setSubDomain(string memory subNodeLabel, address owner, uint256 duration)  
+        external 
+        payable 
+        isApprovedLabel(subNodeLabel)
+        isAvailableLabel(subNodeLabel)
+    {
+        uint32 fuses = 65537; //fuse set to patent cannot control or cannot unwrap
+        uint64 timestamp = uint64(block.timestamp);
+        uint256 parentNodeYrsLeft = getParentExpiry() - timestamp;
+        uint64 maxYears = uint64(parentNodeYrsLeft) / uint64(31556926);
+        uint256 price = getLetterFees(subNodeLabel, duration);
+
+        require(duration <= maxYears,'cant extend date past the parent');
+        require(nameWrapper.isWrapped(parentNode), 'parent must be wrapped');
+        require(nameWrapper.allFusesBurned(parentNode, 1), 'parent must be locked');
+        require(price == msg.value, 'price not correct');
+
+        uint64 subscriptionPeriod = uint64(duration) * 31556926;
+        nameWrapper.setSubnodeRecord(parentNode, subNodeLabel, owner, ensResolver, 0, fuses, subscriptionPeriod + timestamp);
+
+    }
+
+// might not be necesarry to use contract for this owner subdomain cannot be managed by main and therefore more need for proxy // letss see //might be need after all using payable fu
+// the real issue here is we cannot emancipate or risk losing out on yearly subdomain subs model
+// we have to lock instead
+// solved
+    function extendSubDomain(bytes32 subNode,  uint256 duration)
+        external 
+        payable  
+        isNodeActiveOwnerorApproved(subNode) 
+    {
+        string memory label = StringUtils.extractLabel(nameWrapper.names(subNode));
+        bytes32 labelhash = keccak256(bytes(label));
+        uint256 tokenId = uint256(subNode);
+
+        (/* address owner */, /*uint32 fuses*/, uint64 expiry) = nameWrapper.getData(tokenId);
+        //uint256 expires = baseRegistrarImplementation.nameExpires(tokenId);
+        uint256 renewRange = getParentExpiry() - expiry;
+        uint256 maxYears = (renewRange) / (31556926);
+        uint256 price = getLetterFees(label, duration);
+        
+        require(maxYears >= 1 && duration <= maxYears, 'cant extend date past the parent');
+        require(price == msg.value, 'price not correct');
+        uint64 subscriptionPeriod = uint64(duration) * 31556926;
+        
+        nameWrapper.extendExpiry(parentNode, labelhash, subscriptionPeriod + expiry);  
+    }
+
+
+    /*
+    * Withdraw funds
+    */
+    function withdraw() 
+        external 
+        onlyOwner 
+        nonReentrant
+    {
+        (bool success, ) = owner().call{value: address(this).balance}(""); 
+        require(success, "Transfer failed");
+    }
+}
